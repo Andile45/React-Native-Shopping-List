76430ee3670903f4b6b6988435aebf3448b2e517
Andile45 <manganyeandile@gmail.com>
2026-01-13 15:55:51 +0200

    i added the home enntry screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,5 @@
-import { Text, View } from "react-native";
+import ShoppingListScreen from "@/src/screens/ShoppingListScreen";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <ShoppingListScreen/>;
 }
